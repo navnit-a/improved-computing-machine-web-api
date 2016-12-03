7d42ee2038b8901241c627fa03819a66acb3b381
Navnit Anuth <nanuth@qaworks.com>
2016-12-03 13:44:11 +0000

    Initial Commit of the API
    Initial Commit

---
+++ NextGen.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NextGen.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ NextGen.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NextGen.API.WebApiApplication" Language="C#" %>
